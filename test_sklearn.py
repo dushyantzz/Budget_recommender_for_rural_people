9d9caa35aa2f46a286ddf250c4cf2b0506920c25
dushyantzz <157586156+dushyantzz@users.noreply.github.com>
2025-01-31 11:30:08 +0530

    changes committed

--- test_sklearn.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-
-X = np.array([[1, 2], [2, 3], [3, 4]])
-y = np.array([0, 1, 0])
-
-model = LogisticRegression()
-model.fit(X, y)
-print("Model trained successfully!")
